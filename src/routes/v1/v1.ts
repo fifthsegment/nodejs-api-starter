f61d0a9f3e81f9ef5d9cbccc5308109ee3467671
Abdullah Irfan <abdullah.irfan1@gmail.com>
2024-09-15 22:57:45 +0200

    api updates

--- src/routes/v1/v1.ts
+++ src/routes/v1/v1.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import dashboards from './dashboards/dashboards.js';
+import auth from './auth/auth.js';
 
 const router = express.Router();
 router.use('/dashboards', dashboards);
+router.use('/auth', auth);
 
 export default router;
